d904c43e0f433b622f82cc11bf0db6d9e533b9a1
Diego Lopes <38112167+diglopes@users.noreply.github.com>
2025-02-04 18:39:44 -0300

    add class 1 from module 5 (generators and iterators)

---
+++ module-5/class-1-generatos-iterators/src/generators-iterators.js
@@ -0,0 +1,69 @@
+import fs from 'fs'
+import readline from 'readline'
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {Generator<number, number>}
+ */
+export function* calculation (a, b) {
+    yield a * b
+}
+
+
+/**
+ * @returns {Generator<number, string>}
+ */
+export  function* exampleGenerator() {
+    yield "Hello"
+    yield "World"
+    yield* calculation(2, 20)
+}
+
+
+/**
+ * @returns {Generator<number>}
+ */
+export function* promisified() {
+    yield Promise.resolve(1)
+    yield Promise.resolve(2)
+}
+
+/**
+ * 
+ * @param {Array<string>} arr 
+ * @returns {Iterable<string>}
+ */
+export function manualInterator(arr) {
+    let index = 0
+
+    return {
+        next: function() {
+            if(index < arr.length) {
+                return { value: arr[index++], done: false}
+            } else  {
+                return { done: true }
+            }
+        }
+    }
+}
+
+export async function* csvReader(file) {
+    const fileStream = fs.createReadStream(file)
+    const rl = readline.createInterface({ input: fileStream })
+    let headers 
+
+    for await (const line of rl) {
+        if(!headers) 
+            headers = line.split(',')
+        else {
+            const values = line.split(',')
+            yield headers.reduce((acc, cur, index) => {
+                acc[cur] = values[index]
+                return acc
+            }, {})
+        }
+             
+    }
+}
